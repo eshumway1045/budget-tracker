4f41b8a17d51c7f98c77f68c32eb0938c6374729
Eric Shumway <ericshumway2@mail.weber.edu>
2022-06-18 12:24:22 -0600

    changes to server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3002;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/transactions";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/transactions";
 console.log(MONGODB_URI);
 const app = express();
 
